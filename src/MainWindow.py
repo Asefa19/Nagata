eed51c1512fbda5649dbb315e7155d89bac2ad94
Asefa19 <adamsefa@gmail.com>
2024-11-25 01:23:54 +0000

    setting up widgets. Added logo, chat history, asset directory. Messing around with positioning of widgets aswell.

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -1,22 +1,17 @@
 from PySide6.QtCore import QSize, Qt
-from PySide6.QtWidgets import QMainWindow, QDockWidget, QWidget, QPushButton
-from DragButton import DragButton
+from PySide6.QtWidgets import QMainWindow, QDockWidget, QWidget, QLabel
+from PySide6.QtGui import QPixmap
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Nagata")
-        self.setMinimumSize(QSize(800,600))
+        self.setMinimumSize(QSize(1920,1080))
         self.setMaximumSize(QSize(3440,1440))
         self.setBaseSize(QSize(1920,1080))
-        self.setWindowFlags(Qt.FramelessWindowHint)
-        self.setAttribute(Qt.WA_TranslucentBackground)
+        # self.setWindowFlags(Qt.FramelessWindowHint)
+        # self.setAttribute(Qt.WA_TranslucentBackground)
         
-        dockWidget = QDockWidget(self)
-        dockTitle = QWidget(dockWidget)
-        dockWidget.setTitleBarWidget(dockTitle)
-        button = DragButton("Drag", dockWidget)
-        dockWidget._button = DragButton(button)
-
+        
     
--- src/overlay.py
+++ src/overlay.py
@@ -1,8 +1,50 @@
 import sys
 from MainWindow import MainWindow
-from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import Qt, QSize
+from PySide6.QtGui import QPixmap, QImage
+from PySide6.QtWidgets import QApplication, QDockWidget, QWidget, QLabel, QVBoxLayout
 app = QApplication([])
 window = MainWindow()
-window.show()
 
+screen_Bounds = app.primaryScreen().geometry()
+
+dockWidget = QDockWidget(window)
+# dockWidget.setFixedSize(75, 75)
+dockWidget.setGeometry(0,0, 200, 1080)
+dockTitle = QWidget(dockWidget) 
+dockWidget.setTitleBarWidget(dockTitle) 
+dockWidget.setAllowedAreas(Qt.LeftDockWidgetArea)
+# dockWidget.toggleViewAction()
+# dockWidget.setFloating(True)
+
+trayIcon = "/Nagata/Nagata/assets/img/nagata_logo_40x39.png"
+lbl_Logo = QLabel(dockWidget)
+pixmap = QPixmap(trayIcon)
+
+image = pixmap.toImage()
+# size = QSize(114,110)
+# scaled_image = image.scaled(size, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+scaled_pixmap = QPixmap.fromImage(image)
+
+if pixmap.isNull():
+    print(f"Failed to load image from {trayIcon}")
+
+else:
+    lbl_Logo.setPixmap(scaled_pixmap)
+    lbl_Logo.setAlignment(Qt.AlignCenter)
+    # lbl_Logo.setGeometry(0,0,75,75)
+
+lbl_ChatHistory = QLabel(window)
+lbl_ChatHistory.setStyleSheet("background-color: white;")
+# lbl_ChatHistory.move(screen_Bounds.right()-window.width(), screen_Bounds.bottom()-window.height())
+
+dock_Layout = QVBoxLayout()
+dock_Layout.addWidget(lbl_Logo)
+dockWidget.setLayout(dock_Layout)
+
+layout = QVBoxLayout()
+layout.addWidget(lbl_ChatHistory)
+window.setLayout(layout)
+
+window.show()
 app.exec()
