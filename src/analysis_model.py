e3b962d79a8879b7bf84805054d61b8994cd88ad
Asefa19 <adamsefa@gmail.com>
2024-12-23 17:54:36 +0000

    toying with params

--- src/analysis_model.py
+++ src/analysis_model.py
@@ -6,10 +6,12 @@ model_gguf="/Nagata/model/astrollama-3-8b-chat_summary.Q8_0.gguf"
 # Set gpu_layers to the number of layers to offload to GPU. Set to 0 if no GPU acceleration is available on your system.
 llm = Llama(
   model_path=model_gguf,  # Download the model file first
-  n_ctx=1024,  # The max sequence length to use - note that longer sequence lengths require much more resources
+  n_ctx=512,  # The max sequence length to use - note that longer sequence lengths require much more resources
   n_threads=16,            # The number of CPU threads to use, tailor to your system and the resulting performance
-  n_gpu_layers=35         # The number of layers to offload to GPU, if you have GPU acceleration available
+  n_gpu_layers=50,         # The number of layers to offload to GPU, if you have GPU acceleration available
+  chat_format="llama-2"
 )
+# llm = Llama(model_path=model_gguf, chat_format="llama-2")  # Set chat_format according to the model you are using
 
 # Simple inference example
 output = llm(
@@ -21,7 +23,6 @@ output = llm(
 
 # Chat Completion API
 
-llm = Llama(model_path=model_gguf, chat_format="llama-2")  # Set chat_format according to the model you are using
 response = llm.create_chat_completion(
     messages = [
         {"role": "system", "content": "You are a chatbot / analysis assistant."},
