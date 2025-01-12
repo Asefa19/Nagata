fa989c52971bdb081067af0706b5511d41e629fb
Asefa19 <adamsefa@gmail.com>
2025-01-12 08:09:30 +0000

    closer to setting up response function. Need to move Llama statement to main and go from there.

--- src/main.py
+++ src/main.py
@@ -1,34 +1,47 @@
 from analysis_model import *
 from report_model import *
 from transcription import *
+from llama_cpp import Llama
 # Setting up new directory
 #
-selected_Model = 0
+class ModelSelection():
 
-match selected_Model:
-    case 0:
-        model = analysis_model("/Nagata/model/astrollama-3-8b-chat_summary.Q8_0.gguf")
-        while(True):
-            # have a conversation
-                # look for user input
-            user_VoiceInput = sr.Recognizer()
-            transcription = transcribe(user_VoiceInput)
-            print(transcription)
-            user_TextInput = transcription
-                # pass to model content
+    def select():
+        selected_Model = 0
+        match selected_Model:
+            case 0:
+                model = analysis_model("/Nagata/model/astrollama-3-8b-chat_summary.Q8_0.gguf")
+                role = "Data Analyst for Astronomy"
+                
+                while(True):
+                    user_TextInput = ModelSelection.listen()
+                        # pass to model content
+                    rsp = ModelSelection.response(user_TextInput, role, model)
+                    # send rsp to chat label
+                
+            case 1:
+                model = report_Model("/Nagata/model/astrollama-3-8b-chat_summary.Q8_0.gguf")
+                role = "Report writer and critic for Astronomy"
+                
+                while(True):
+                    user_TextInput = ModelSelection.listen()
+                    rsp = ModelSelection.response(user_TextInput, role, model)
             
-                # print response to chat label
-        
-    case 1:
-        model = report_Model("/Nagata/model/astrollama-3-8b-chat_summary.Q8_0.gguf")
 
-def response(content, role):
-    response = model.llm.create_chat_completion(
-      messages = [
-          {"role": "system", "content": f"You are a {role}"},
-          {
-              "role": "user",
-              "content": f"{content}"
-          }
-      ]
-  )
+    def response(content, role, model):
+        rsp = model.llm.create_chat_completion(
+        messages = [
+            {"role": "system", "content": f"You are a {role}"},
+                {
+                "role": "user",
+                "content": f"{content}"
+                }
+            ]
+        )
+        return rsp
+    
+    def listen():
+        user_VoiceInput = sr.Recognizer()
+        transcription = transcribe(user_VoiceInput)
+        print(transcription)
+        return transcription
